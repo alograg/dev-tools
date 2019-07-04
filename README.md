1c8a3cca694250baa21d6260292d9fd37b000718
alograg <alograg@gmail.com>
2019-07-04 23:15:58 +0200

    build(core): Shell tools

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Alograg DevTools
 
+[![Latest Stable Version](https://poser.pugx.org/alograg/dev-tools/v/stable.svg)](https://packagist.org/packages/alograg/dev-tools)
+[![Total Downloads](https://poser.pugx.org/alograg/dev-tools/downloads.svg)](https://packagist.org/packages/alograg/dev-tools)
+[![License](https://poser.pugx.org/alograg/dev-tools/license.svg)](https://packagist.org/packages/alograg/dev-tools)
+
 Dev tools for Lumen/Laravel projects
 
 Based on:
---
+++ change-log-builder.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+previous_tag=0
+for current_tag in $(git tag --sort=-creatordate)
+do
+
+if [ "$previous_tag" != 0 ];then
+    tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
+    printf "## ${previous_tag} (${tag_date})\n\n"
+    git log ${current_tag}...${previous_tag} --pretty=format:'*  %s [View](https://bitbucket.org/projects/test/repos/my-project/commits/%H)' --reverse | grep -v Merge
+    printf "\n\n"
+fi
+previous_tag=${current_tag}
+done
+# git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"%s" | grep -i -E "^(\[INTERNAL\]|\[FEATURE\]|\[FIX\]|\[DOC\])*\[FEATURE\]"
+#git log `git describe --tags --abbrev=0`..HEAD --pretty=format:"  * %s"
+#git log --all --format="%aN <%aE>" | sort -u
