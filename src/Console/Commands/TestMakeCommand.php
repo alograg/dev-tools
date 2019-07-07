d8e780fb97fa22104f6a3ebcf8abe0d8110c3d8c
alograg <alograg@gmail.com>
2019-07-07 19:01:27 +0200

    feat(core): Review test

--- src/Console/Commands/TestMakeCommand.php
+++ src/Console/Commands/TestMakeCommand.php
@@ -1,23 +1,27 @@
 <?php
-
 namespace Alograg\DevTools\Console\Commands;
 
+use Alograg\DevTools\Abstracts\Maker;
+use Alograg\DevTools\Traits\MakerTrait;
 use Illuminate\Support\Str;
-use Illuminate\Console\GeneratorCommand;
 use InvalidArgumentException;
 
-class TestMakeCommand extends GeneratorCommand
+class TestMakeCommand extends Maker
 {
+    use MakerTrait;
     /**
      * The console command name.
      *
      * @var string
      */
-    protected $signature = 'make:test {name : The name of the class} '.
-        '{--unit : Create a unit test} '.
-        '{--parent= : Generate a nested resource controller test} '.
-        '{--model= : Generate a resource controller test for the given model} '.
-        '{--resource : Generate a resource controller test}';
+    protected $signature = <<<TEXT
+make:test {name : The name of the class} 
+    {--unit : Create a unit test}
+    {--parent= : Generate a nested resource controller test}
+    {--model= : Generate a resource controller test for the given model}
+    {--resource : Generate a resource controller test}
+    {--sp|stub-path : Path relative a stubs repo}
+TEXT;
 
     /**
      * The console command description.
@@ -34,65 +38,46 @@ class TestMakeCommand extends GeneratorCommand
     protected $type = 'Test';
 
     /**
-     * Get the stub file for the generator.
-     *
-     * @return string
-     */
-    protected function getStub()
-    {
-        if ($this->option('unit')) {
-            return __DIR__.'/stubs/unit-test.stub';
-        }
-
-        if ($this->option('parent')) {
-            return __DIR__.'/stubs/test.nested.stub';
-        } elseif ($this->option('model')) {
-            return __DIR__.'/stubs/test.model.stub';
-        } elseif ($this->option('resource')) {
-            return __DIR__.'/stubs/test.stub';
-        }
-
-        return __DIR__.'/stubs/test.plain.stub';
-    }
-
-    /**
      * Get the destination class path.
      *
-     * @param  string  $name
+     * @param string $name
+     *
      * @return string
      */
     protected function getPath($name)
     {
         $name = Str::replaceFirst($this->rootNamespace(), '', $name);
 
-        return base_path('tests').str_replace('\\', '/', $name).'.php';
+        return base_path('tests') . str_replace('\\', '/', $name) . '.php';
     }
 
     /**
      * Get the destination class app path.
      *
-     * @param  string  $name
+     * @param string $name
+     *
      * @return string
      */
     protected function getAppPath($name)
     {
         $name = Str::replaceFirst($this->laravel->getNamespace(), '', $name);
 
-        return $this->laravel['path'].'/'.str_replace('\\', '/', $name).'.php';
+        return $this->laravel['path'] . '/' . str_replace('\\', '/', $name) . '.php';
     }
 
     /**
      * Get the default namespace for the class.
      *
-     * @param  string  $rootNamespace
+     * @param string $rootNamespace
+     *
      * @return string
      */
     protected function getDefaultNamespace($rootNamespace)
     {
         if ($this->option('unit')) {
-            return $rootNamespace.'\Unit';
+            return $rootNamespace . '\Unit';
         } else {
-            return $rootNamespace.'\Feature';
+            return $rootNamespace . '\Feature';
         }
     }
 
@@ -101,58 +86,58 @@ class TestMakeCommand extends GeneratorCommand
      *
      * Remove  the base controller import if we are already in base namespace.
      *
-     * @param  string $name
+     * @param string $name
+     *
      * @return string
      * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
     protected function buildClass($name)
     {
         $replace = [];
-
         if ($this->option('parent')) {
             $replace = $this->buildParentReplacements();
         }
-
         if ($this->option('model')) {
             $replace = $this->buildModelReplacements($replace);
             if ($this->option('parent')) {
                 $replace = array_merge($replace, [
-                    'dummy_action_index' => 'route(\''.$this->getRouteName($name).'.index\', [ \''.$replace['parent_dummy_model_variable'].'\' => $'.$replace['parent_dummy_model_variable'].'->getKey() ])',
-                    'dummy_action_store' => 'route(\''.$this->getRouteName($name).'.store\', [ \''.$replace['parent_dummy_model_variable'].'\' => $'.$replace['parent_dummy_model_variable'].'->getKey() ])',
-                    'dummy_action_show' => 'route(\''.$this->getRouteName($name).'.show\', [ \''.$replace['parent_dummy_model_variable'].'\' => $'.$replace['parent_dummy_model_variable'].'->getKey(), \''.$replace['dummy_model_variable'].'\' => $'.$replace['dummy_model_variable'].'->getKey() ])',
-                    'dummy_action_update' => 'route(\''.$this->getRouteName($name).'.update\', [ \''.$replace['parent_dummy_model_variable'].'\' => $'.$replace['parent_dummy_model_variable'].'->getKey(), \''.$replace['dummy_model_variable'].'\' => $'.$replace['dummy_model_variable'].'->getKey()  ])',
-                    'dummy_action_destroy' => 'route(\''.$this->getRouteName($name).'.destroy\', [ \''.$replace['parent_dummy_model_variable'].'\' => $'.$replace['parent_dummy_model_variable'].'->getKey(), \''.$replace['dummy_model_variable'].'\' => $'.$replace['dummy_model_variable'].'->getKey()  ])',
+                    'dummy_action_index'   => 'route(\'' . $this->getRouteName($name) . '.index\', [ \'' . $replace['parent_dummy_model_variable'] . '\' => $' . $replace['parent_dummy_model_variable'] . '->getKey() ])',
+                    'dummy_action_store'   => 'route(\'' . $this->getRouteName($name) . '.store\', [ \'' . $replace['parent_dummy_model_variable'] . '\' => $' . $replace['parent_dummy_model_variable'] . '->getKey() ])',
+                    'dummy_action_show'    => 'route(\'' . $this->getRouteName($name) . '.show\', [ \'' . $replace['parent_dummy_model_variable'] . '\' => $' . $replace['parent_dummy_model_variable'] . '->getKey(), \'' . $replace['dummy_model_variable'] . '\' => $' . $replace['dummy_model_variable'] . '->getKey() ])',
+                    'dummy_action_update'  => 'route(\'' . $this->getRouteName($name) . '.update\', [ \'' . $replace['parent_dummy_model_variable'] . '\' => $' . $replace['parent_dummy_model_variable'] . '->getKey(), \'' . $replace['dummy_model_variable'] . '\' => $' . $replace['dummy_model_variable'] . '->getKey()  ])',
+                    'dummy_action_destroy' => 'route(\'' . $this->getRouteName($name) . '.destroy\', [ \'' . $replace['parent_dummy_model_variable'] . '\' => $' . $replace['parent_dummy_model_variable'] . '->getKey(), \'' . $replace['dummy_model_variable'] . '\' => $' . $replace['dummy_model_variable'] . '->getKey()  ])',
                 ]);
             } else {
                 $replace = array_merge($replace, [
-                    'dummy_action_index' => 'route(\''.$this->getRouteName($name).'.index\')',
-                    'dummy_action_store' => 'route(\''.$this->getRouteName($name).'.store\')',
-                    'dummy_action_show' => 'route(\''.$this->getRouteName($name).'.show\', [ \''.$replace['dummy_model_variable'].'\' => $'.$replace['dummy_model_variable'].'->getKey() ])',
-                    'dummy_action_update' => 'route(\''.$this->getRouteName($name).'.update\', [ \''.$replace['dummy_model_variable'].'\' => $'.$replace['dummy_model_variable'].'->getKey() ])',
-                    'dummy_action_destroy' => 'route(\''.$this->getRouteName($name).'.destroy\', [ \''.$replace['dummy_model_variable'].'\' => $'.$replace['dummy_model_variable'].'->getKey() ])',
+                    'dummy_action_index'   => 'route(\'' . $this->getRouteName($name) . '.index\')',
+                    'dummy_action_store'   => 'route(\'' . $this->getRouteName($name) . '.store\')',
+                    'dummy_action_show'    => 'route(\'' . $this->getRouteName($name) . '.show\', [ \'' . $replace['dummy_model_variable'] . '\' => $' . $replace['dummy_model_variable'] . '->getKey() ])',
+                    'dummy_action_update'  => 'route(\'' . $this->getRouteName($name) . '.update\', [ \'' . $replace['dummy_model_variable'] . '\' => $' . $replace['dummy_model_variable'] . '->getKey() ])',
+                    'dummy_action_destroy' => 'route(\'' . $this->getRouteName($name) . '.destroy\', [ \'' . $replace['dummy_model_variable'] . '\' => $' . $replace['dummy_model_variable'] . '->getKey() ])',
                 ]);
             }
         }
-
         $replace['dummy_route'] = $this->getRouteName($name);
 
-        return str_replace(array_keys($replace), array_values($replace), $this->replaceUserNamespace(parent::buildClass($name)));
+        return str_replace(array_keys($replace), array_values($replace),
+            $this->replaceUserNamespace(parent::buildClass($name)));
     }
 
     /**
      * Replace the User model namespace.
      *
-     * @param  string  $stub
+     * @param string $stub
+     *
      * @return string
      */
     protected function replaceUserNamespace($stub)
     {
-        if (! config('auth.providers.users.model')) {
+        if (!config('auth.providers.users.model')) {
             return $stub;
         }
 
         return str_replace(
-            $this->rootNamespace().'User',
+            $this->rootNamespace() . 'User',
             config('auth.providers.users.model'),
             $stub
         );
@@ -168,23 +153,28 @@ class TestMakeCommand extends GeneratorCommand
         $parentModelClass = $this->parseModel($this->option('parent'));
         if (!$this->files->exists($this->getAppPath($parentModelClass))) {
             if ($this->confirm("A {$parentModelClass} model does not exist. Do you want to generate it?", true)) {
-                $this->call('make:model', ['name' => str_replace($this->laravel->getNamespace(), '', $parentModelClass), '-m' => true, '-f' => true]);
+                $this->call('make:model', [
+                    'name' => str_replace($this->laravel->getNamespace(), '', $parentModelClass),
+                    '-m'   => true,
+                    '-f'   => true,
+                ]);
             }
         }
 
         return [
-            'ParentDummyFullModelClass' => $parentModelClass,
-            'ParentDummyModelClass' => class_basename($parentModelClass),
-            'ParentDummyModelVariable' => lcfirst(class_basename($parentModelClass)),
+            'ParentDummyFullModelClass'   => $parentModelClass,
+            'ParentDummyModelClass'       => class_basename($parentModelClass),
+            'ParentDummyModelVariable'    => lcfirst(class_basename($parentModelClass)),
             'parent_dummy_model_variable' => snake_case(class_basename($parentModelClass)),
-            'ParentDummyTitle' => ucwords(snake_case(class_basename($parentModelClass), ' ')),
+            'ParentDummyTitle'            => ucwords(snake_case(class_basename($parentModelClass), ' ')),
         ];
     }
 
     /**
      * Build the model replacement values.
      *
-     * @param  array  $replace
+     * @param array $replace
+     *
      * @return array
      */
     protected function buildModelReplacements(array $replace)
@@ -192,25 +182,30 @@ class TestMakeCommand extends GeneratorCommand
         $modelClass = $this->parseModel($this->option('model'));
         if (!$this->files->exists($this->getAppPath($modelClass))) {
             if ($this->confirm("A {$modelClass} model does not exist. Do you want to generate it?", true)) {
-                $this->call('make:model', ['name' => str_replace($this->laravel->getNamespace(), '', $modelClass), '-m' => true, '-f' => true]);
+                $this->call('make:model', [
+                    'name' => str_replace($this->laravel->getNamespace(), '', $modelClass),
+                    '-m'   => true,
+                    '-f'   => true,
+                ]);
             }
         }
 
         return array_merge($replace, [
-            'DummyFullModelClass' => $modelClass,
-            'DummyModelClass' => class_basename($modelClass),
-            'DummyModelVariable' => lcfirst(class_basename($modelClass)),
-            'dummyModelVariable' => camel_case(class_basename($modelClass)),
-            'dummy_model_variable' => snake_case(class_basename($modelClass)),
+            'DummyFullModelClass'         => $modelClass,
+            'DummyModelClass'             => class_basename($modelClass),
+            'DummyModelVariable'          => lcfirst(class_basename($modelClass)),
+            'dummyModelVariable'          => camel_case(class_basename($modelClass)),
+            'dummy_model_variable'        => snake_case(class_basename($modelClass)),
             'dummy_model_plural_variable' => str_plural(snake_case(class_basename($modelClass))),
-            'DummyTitle' => ucwords(snake_case(class_basename($modelClass), ' ')),
+            'DummyTitle'                  => ucwords(snake_case(class_basename($modelClass), ' ')),
         ]);
     }
 
     /**
      * Get the fully-qualified model class name.
      *
-     * @param  string  $model
+     * @param string $model
+     *
      * @return string
      */
     protected function parseModel($model)
@@ -218,11 +213,9 @@ class TestMakeCommand extends GeneratorCommand
         if (preg_match('([^A-Za-z0-9_/\\\\])', $model)) {
             throw new InvalidArgumentException('Model name contains invalid characters.');
         }
-
         $model = trim(str_replace('/', '\\', $model), '\\');
-
-        if (! Str::startsWith($model, $rootNamespace = $this->laravel->getNamespace())) {
-            $model = $rootNamespace.$model;
+        if (!Str::startsWith($model, $rootNamespace = $this->laravel->getNamespace())) {
+            $model = $rootNamespace . $model;
         }
 
         return $model;
@@ -231,12 +224,13 @@ class TestMakeCommand extends GeneratorCommand
     /**
      * Get the route name.
      *
-     * @param  string  $name
+     * @param string $name
+     *
      * @return string
      */
     protected function getRouteName($name)
     {
-        $name = Str::replaceFirst($this->getDefaultNamespace(trim($this->rootNamespace(), '\\')).'\\', '', $name);
+        $name = Str::replaceFirst($this->getDefaultNamespace(trim($this->rootNamespace(), '\\')) . '\\', '', $name);
         $name = Str::replaceLast('ControllerTest', '', $name);
         $names = explode('\\', $name);
         foreach ($names as $key => $value) {
